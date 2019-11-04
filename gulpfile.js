c07f14792cacc0d4039c356e33ab8da05d775bbe
Thomas Vagning <thv@kamstrup.com>
2019-11-04 11:06:56 +0100

    Add gulp task styles

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,10 @@ const {src, dest, series} = require('gulp');
 const concat = require('gulp-concat');
 const uglify = require('gulp-uglify');
 const rename = require('gulp-rename');
+const sass = require('gulp-sass');
+const cleanCSS = require('gulp-clean-css');
 
+//Function to concatinate the given JS files into one files call all.js and save it in the js dir
 function concatJS(cb) {
 	return src([
 		'js/circle/autogrow.js',
@@ -12,9 +15,10 @@ function concatJS(cb) {
 	.pipe(concat('all.js'))
 	.pipe(dest('js'));
 
- 	cb();
+	cb()
 }
 
+//Function for minifying the all.js file and renaming it to all.min.js and saving it in the dist/scripts dir
 function minifyJS(cb) {
 	src('js/all.js')
 	.pipe(uglify())
@@ -24,5 +28,27 @@ function minifyJS(cb) {
 	cb();
 }
 
+//Function to compile Sass into CSS and saving it in css dir
+function compileSass(cb) {
+	return src('sass/global.scss')
+	.pipe(sass())
+	.pipe(dest('css'))
+
+	cb()
+}
+
+//Function for minifying the global.css file and renaming it to all.min.css and saving it in the dist/styles dir
+function minifyCSS(cb) {
+	src('css/global.css')
+	.pipe(cleanCSS())
+	.pipe(rename('all.min.css'))
+	.pipe(dest('dist/styles'));
+
+	cb();
+}
+
+//Creating a gulp task called scrips which first runs concatJS function, then minifyJS function
 exports.scripts = series(concatJS, minifyJS);
 
+//Creating a gulp task called styles which first runs compileSass function, then minifyCSS function
+exports.styles = series(compileSass, minifyCSS);
